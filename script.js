efe3fc02f3fd074dddae271daa583a1277517e67
fdemirciler <fdemirciler@gmail.com>
2021-06-18 00:39:17 +0200

    d

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const btn = document.getElementById('button');
 
 btn.addEventListener('click', getNumber);
 
-const canvas = document.getElementsByClassName('container')[0];
+const canvas = document.querySelector('.container');
 
+function gridSize(num) {
+    canvas.style.setProperty("--grid-rows", num);
+    canvas.style.setProperty("--grid-columns", num);
+}
 
 // User input and grid draw
 
@@ -30,30 +34,64 @@ function getNumber() {
 
 function drawGrid(num) {
 
-    for (let i = 1; i <= num; i++) {
+    for (let i = 1; i <= num * num; i++) {
 
         let rowdiv = document.createElement('div');
         rowdiv.className = 'rows';
-
-        for (let j = 1; j <= num; j++) {
-
-            let coldiv = document.createElement('div');
-            coldiv.className = 'columns';
-            // coldiv.textContent = j;
-            rowdiv.appendChild(coldiv);
-        }
-
         canvas.appendChild(rowdiv);
+        gridSize(num)
+
     }
+
+    // //setColor()
+}
+
+
+function randomColor() {
+
+    return Math.floor(Math.random() * 16777215).toString(16);
 
 }
 
-// reset grid
+document.documentElement.style.setProperty('--bg-color',randomColor());
 
-// function holdOn() {
-//     window.location.reload()
-//     return Promise.resolve().then( () => {
-//         getNumber()
+
+function setColor() {
+    let cells = document.querySelectorAll(".rows");
+
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("mouseover", function (e) {
+            console.log(i)
+            e.target.style.backgroundColor = randomColor();
+        });
+    }
+}
+
+// const cells = document.getElementsByClassName('rows');
+// [].forEach.call(cells, function (p) {
+//     p.addEventListener('mouseover', function (e) {
+//         console.log(e)
 //     })
+// })
 
-// }
+
+// Array.from(document.getElementsByClassName('columns')).forEach(el => {
+
+//     el.innerHTML = 'X'
+
+//     // .style.setProperty('--background-color', randomColor)
+
+// })
+
+// document.getElementsByClassName('columns').forEach(function (cell) {
+
+//     cell.addEventListener('mouseover', function (e) {
+//         e.style.backgroundColor = randomColor()
+//     })
+// })
+
+// window.onload = function() {
+//     setTimeout(function() {
+//       document.documentElement.style.cssText = "--main-background-color: red";
+//     }, 2000);
+//   }
